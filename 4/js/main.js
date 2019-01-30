3d26ac818dfb92cc85a585ac7d1f8b6871a68379
Ivan Lyahnovich <vanya.lyacnovich@mail.ru>
2019-01-30 19:32:57 +0600

    f

---
+++ 4/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){ 
+	$('form').submit(function(event){ 
+
+		event.preventDefault(); 
+
+		$.ajax({ 
+			url:$(this).attr('action'), 
+			type: $(this).attr('method'), 
+			data: new FormData(this), 
+			contentType:false, 
+			cache:false, 
+			processData:false, 
+			success: function(result){ 
+				$('#content').html(result); 
+			}, 
+		}); 
+	}); 
+});
+
+function fun1() { 
+var a=document.getElementById('r'); 
+var b=document.getElementById('one'); 
+b.innerHTML=a.value; 
+}
